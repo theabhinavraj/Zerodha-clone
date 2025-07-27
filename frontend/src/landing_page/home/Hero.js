2b5a59edcadbabb4238ccdc23a7ca9667b0fe361
Abhinav Raj <abhinav2abhinav1234@gmail.com>
2025-07-28 00:34:08 +0530

    small change

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
             <div className='row text-center'>
                 <img src='\media\images\homeHero.png' alt='Hero image' className='mb-5' />
                 <h1 className='mt-5'>Invest in everything</h1>
-                <p>Online platform to invest in stocks,derivatives,mutual funds,and more</p>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, and more.</p>
                 <button className='p-3 btn btn-primary fs-5 mb-5' style={{width:"20%", margin:"0 auto"}}>Signup Now</button>
             </div>
         </div>
